1474e9f1d9725ce58fcf0108d0f4a073b5602ae7
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-03-02 15:47:39 +0530

    vowelc.c
    vowel

---
+++ vowelc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char k;
+scanf("%c",&k);
+if(k=='a' || k=='e'|| k=='i'|| k=='o' || k=='u' || k=='A' || k=='E'|| k=='I'|| k=='O' || k=='U')
+{
+printf("The char is vowel");
+}
+else
+printf("The char is consonent");
+return 0;
+}
